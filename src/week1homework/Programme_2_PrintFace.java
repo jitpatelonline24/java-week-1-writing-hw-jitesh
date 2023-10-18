83edd3e7065de33dfe63279a2b3ab7aa697343de
jitpatelonline24 <jitpatelonline24@gmail.com>
2023-10-18 23:27:06 +0100

    This is written java homework

---
+++ src/week1homework/Programme_2_PrintFace.java
@@ -0,0 +1,15 @@
+package week1homework;
+
+public class Programme_2_PrintFace {
+    /**
+     * 2. Write a Java program to print a face.
+     */
+    public static void main(String[] args) {
+        System.out.println(" +--------+");
+        System.out.println("[|  0 0   |] ");
+        System.out.println(" |   ^    |  ");
+        System.out.println(" |  '_'   |  ");
+        System.out.println(" +_______+|  ");
+
+    }
+}
